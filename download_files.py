7222838884d9d60a218519c5b1fa5afab9d06c6b
Brian Krog <brk@specialisterne.com>
2024-11-08 15:27:50 +0100

    Initial commit

---
+++ download_files.py
@@ -0,0 +1,73 @@
+import requests
+import asyncio
+import pandas as pd
+
+
+# download_spec_line = [
+# 'BRnum',
+# 'Featured Report?',
+# 'Name',
+# 'GOLD Community',
+# 'Size',
+# 'Organization type',
+# 'Listed/Non-listed',
+# 'Sector',
+# 'Country',
+# 'Country Status',
+# 'Region',
+# 'Date Added',
+# 'Title',
+# 'Publication Year',
+# 'Integrated',
+# 'Type',
+# 'Adherence Level',
+# 'Status',
+# 'GRI Service',
+# 'External Assurance',
+# 'Type of Assurance Provider',
+# 'Assurance Provider',
+# 'Stakeholder Panel/ Expert Opinion',
+# 'Assurance Scope',
+# 'Level of Assurance',
+# 'Assurance Standard: AA1000 AS',
+# 'Assurance Stanadard: ISAE3000',
+# 'Assurance Standard: national (general)',
+# 'Assurance Standard: national (sustainability)',
+# 'Sector Supplements (Final)',
+# 'OECD',
+# 'UNGC',
+# 'CDP',
+# 'IFC',
+# 'ISO',
+# 'SDGs',
+# 'GRI Content Index URL',
+# 'Pdf_URL',
+# 'Report Html Address',
+# 'Report Drt Address',
+# 'Database link'
+# ]
+# download_spec = (download_spec_line for _ in range(21057))
+
+def read_init_status(file):
+    f = open(file,'r')
+    f.
+    return
+
+
+def update_status():
+
+    return
+
+
+def download_file(url):
+
+    return status
+
+
+async def main():
+    xl_input = pd.read_excel('GRI_2017_2020.xlsx')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+    print('Done')
